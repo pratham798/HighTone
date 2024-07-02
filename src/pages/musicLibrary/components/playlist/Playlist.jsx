e66b60f7a4efc156578c72f3dbc43a81b91540f7
pratham798 <prathamjamnal7982@gmail.com>
2024-07-02 20:45:04 +0530

    nit: fix

--- src/pages/musicLibrary/components/playlist/Playlist.jsx
+++ src/pages/musicLibrary/components/playlist/Playlist.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useMusicStore } from '../../../../store/musicStore';
 import { shallow } from "zustand/shallow";
 import SearchIcon from '../../../../assets/search.svg';
@@ -8,17 +8,19 @@ import LoadingState from './LoadingState';
 
 const Playlist = ({songs, isLoading}) => {
   const { setFilterSongs } = useMusicStore((state)=>({setFilterSongs: state.setFilterSongs}), shallow);
-  const filterSongList = (action) => setFilterSongs('top_track', top_track => top_track === action);
+  const [category, setCategory] = useState(null);
+  const filterSongList = (action) => {
+    setFilterSongs('top_track', top_track => top_track === action);
+    setCategory(action);
+  }
 
   return (
     <section className='flex flex-col gap-8 pt-8 max-lg:p-7 max-lg:order-2 min-w-80 flex-1'>
       <header className='flex flex-row gap-10 text-slate-100 font-extrabold text-2xl'>
-        <span className='cursor-pointer opacity-35 hover:opacity-100' onClick={()=>filterSongList(null)}>
-            For You
-        </span>
-        <span className='cursor-pointer opacity-35 hover:opacity-100' onClick={()=>filterSongList(true)}>
-          Top Tracks
-        </span>
+        <span className={`cursor-pointer hover:opacity-100 ${category === null ? 'opacity-100': 'opacity-35'}`} 
+          onClick={()=>filterSongList(null)}> For You </span>
+        <span className={`cursor-pointer hover:opacity-100 ${category === true ? 'opacity-100': 'opacity-35'}`} 
+          onClick={()=>filterSongList(true)}> Top Tracks </span>
       </header>
       <section className='flex flex-row gap-5 bg-white bg-opacity-10 cursor-pointer p-2 rounded-md font-medium'>
         <SearchSong songs={songs} />
